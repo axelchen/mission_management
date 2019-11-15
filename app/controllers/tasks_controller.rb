e1d836cd55d58d7d23592539ad95d968669a90d2
David Chen <davidchen1994520@gmail.com>
2019-11-15 12:32:19 +0800

    fix syntax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,3 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.all
-  end
-
-  def new
-    @tasks = Task.new
-  end
-
-  def create    
-  end
-    
-  def edit    
-  end
+  
 end
